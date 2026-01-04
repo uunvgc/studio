785ce75144169f1131b581feb61f576d5f00e79b
Keith Ross <keithross1205@gmail.com>
2026-01-04 13:13:01 +0000

    5:11 AM I can see from your conversation history that you've been strugg

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -5,7 +5,6 @@ import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
-import { ChevronLeft, ChevronRight } from "lucide-react"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
@@ -52,10 +51,6 @@ function Calendar({
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
         day_hidden: "invisible",
         ...classNames,
-      }}
-      components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
