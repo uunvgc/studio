ce5a9676eab5091a35872dc007f092cff479d5af
Keith Ross <keithross1205@gmail.com>
2026-01-04 22:19:34 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  watchOptions: {
-    // This is a workaround for a known issue where the Next.js dev server
-    // can get into a restart loop in some containerized environments.
-    // By ignoring the config file, we prevent the server from restarting
-    // when the file's metadata changes unexpectedly.
-    ignored: ['**/next.config.ts'],
+  webpack: (config) => {
+    config.watchOptions.ignored = [
+        ...config.watchOptions.ignored,
+        '**/next.config.ts',
+    ];
+    return config;
   },
 };
 
