289d4e618748ae3b71bdee3b526ab43979fa51e7
Keith Ross <keithross1205@gmail.com>
2026-01-04 21:46:31 +0000

    the Step 2: DELETE this completely experimental: {   serverActions: {},

--- next.config.ts
+++ next.config.ts
@@ -23,14 +23,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    serverActions: {
-        // By default, the size of the request body sent to a Server Action is limited to 1MB.
-        // You can configure this limit using the serverActions.bodySizeLimit option.
-        // It can be a number in bytes, or a string with a unit (e.g. "2mb").
-        bodySizeLimit: '2mb',
-    },
-  }
 };
 
 export default nextConfig;
