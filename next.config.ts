2a16ed0dedcae80080b9865544c091a4a789ae8f
Keith Ross <keithross1205@gmail.com>
2026-01-04 22:54:40 +0000

    const nextConfig = {   webpack(config, { isServer }) {     // Use spread

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  webpack(config, { isServer }) {
+    // Use spread or new object, DO NOT mutate read-only props
+    config.module.rules = [...config.module.rules];
+    return config;
+  },
+};
 
 export default nextConfig;
