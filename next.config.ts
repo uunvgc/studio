6f8dd314a0305cf3bdf928c0270c64d994fcf99b
Keith Ross <keithross1205@gmail.com>
2026-01-04 22:53:11 +0000

    import type { NextConfig } from "next";
    const nextConfig: NextConfig =

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  webpack: (config) => {
-    // Safely extend the existing ignored paths, or initialize if it doesn't exist.
-    const ignored = Array.isArray(config.watchOptions.ignored)
-      ? config.watchOptions.ignored
-      : [];
-      
-    config.watchOptions.ignored = [
-        ...ignored,
-        '**/next.config.ts',
-    ];
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
