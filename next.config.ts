2cda781d5896136d80f0793576bdf1006fbb4a5d
Keith Ross <keithross1205@gmail.com>
2026-01-05 06:43:29 +0000

    Try fixing this error: `Runtime FirebaseError: Firebase: Error (auth/inv

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,9 @@ const nextConfig: NextConfig = {
 
     return config;
   },
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  },
 };
 
 export default nextConfig;
