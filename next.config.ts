d0b98e695dc62774ffa9d0cbcdf59b2f599887b3
Keith Ross <keithross1205@gmail.com>
2026-01-06 00:43:31 +0000

    Try fixing this error: `Console Error: Firebase API key is missing. Make

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  },
 };
 
 export default nextConfig;
