b7ac6506a37c10d854a96b01c30c419190b83e1a
Keith Ross <keithross1205@gmail.com>
2026-01-06 01:33:59 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
   // available to the browser. The issue was likely due to how the build
   // process was handling the variables. This empty config is sufficient
   // as long as the variable is set in the environment.
+  devIndicators: {
+    allowedDevOrigins: [
+      "9000-firebase-profitpro-ai5-1767600422800.cluster-dccle54vc5d26v2fsixd2icgqi.cloudworkstations.dev",
+    ],
+  },
 };
 
 export default nextConfig;
