071cc2eb0d5f91a502210c801f99438ab67e70b3
Keith Ross <keithross1205@gmail.com>
2026-01-07 12:34:41 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   devIndicators: {
     allowedDevOrigins: [
       "9000-firebase-profitpro-ai5-1767600422800.cluster-dccle54vc5d26v2fsixd2icgqi.cloudworkstations.dev",
+      "6000-firebase-profitpro-ai5-1767600422800.cluster-dccle54vc5d26v2fsixd2icgqi.cloudworkstations.dev",
     ],
   },
 };
