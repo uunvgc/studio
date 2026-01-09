ce0ab72549596c0a5282eab86a2106a9b5d245f6
uunvgc <Keithross1205@gmail.com>
2026-01-09 01:04:47 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
-import "dotenv/config";
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  devIndicators: {
-    allowedDevOrigins: [
-      "9000-firebase-profitpro-ai5-1767600422800.cluster-dccle54vc5d26v2fsixd2icgqi.cloudworkstations.dev",
-      "6000-firebase-profitpro-ai5-1767600422800.cluster-dccle54vc5d26v2fsixd2icgqi.cloudworkstations.dev",
-    ],
-  },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
